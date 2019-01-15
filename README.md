31506a5d9e069f429e9d0fa07bfcfd8567a8cf9f
feliun <felipe.polo@gmail.com>
2019-01-15 17:43:08 +0100

    Adding systemic component to deal with pub/sub on azure bus

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # systemic-azure-bus
-Systemic adapter for azure bus
+A systemic component to deal with azure bus topics and queues subscriptions and publications
+
+A configuration example would be:
+
+```json
+{
+  "bus": {
+		"publications": {
+			"emailSent": {
+				"topic": "email-service.v1.email.sent"
+			},
+			"emailError": {
+				"topic": "email-service.v1.email.error"
+			}
+		},
+		"subscriptions": {
+			"orderPlaced": {
+				"topic": "klopotek.v1.order.confirmed",
+				"subscription": "email.order_confirmed.send"
+			}
+		}
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+const debug = require('debug')('systemic-azure-bus');
+const { creatSubscriptionListener, createTopicSender } = require('@liber/ez-pubsub');
+
+module.exports = () => {
+	const registeredSubscriptions = [];
+
+	const start = async ({ config: { subscriptions, publications }, logger }) => {
+
+		const publish = publicationId => message => {
+			const { topic } = publications[publicationId] || {};
+			if (!topic) throw new Error(`Topic for publication ${publicationId} non found!`);
+			debug(`Preparing connection to publish ${publicationId} on topic ${topic}...`);
+			const sender = createTopicSender({ topic });
+			return sender.send({ message: { body: JSON.stringify(message) } });
+		};
+
+		const subscribe = (onError, onStop) => (subscriptionId, handler) => {
+			const { topic, subscription } = subscriptions[subscriptionId] || {};
+			if (!topic || !subscription) throw new Error(`Data for subscription ${subscriptionId} non found!`);
+			const newSubscription = creatSubscriptionListener({
+				topic,
+				subscription,
+				onMessage: handler,
+				onError: error => {
+					logger.error(error.message);
+					onError(error);
+				},
+				onStop,
+			});
+			registeredSubscriptions.push(newSubscription);
+			debug(`Starting subscription ${subscriptionId} on topic ${topic}...`);
+			newSubscription.start();
+		};
+
+		return {
+			publish,
+			subscribe,
+		};
+	};
+
+	const stop = async () => registeredSubscriptions.forEach(subscription => subscription.stop());
+
+	return { start, stop };
+};
