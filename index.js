bcb49e53ec18bf325a9cb35be26b78115cbca562
feliun <felipe.polo@gmail.com>
2019-03-09 14:36:04 +0100

    Refactor WIP - exp backoff

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const moment = require('moment');
 const debug = require('debug')('systemic-azure-bus');
 const { join } = require('path');
 const requireAll = require('require-all');
@@ -45,49 +44,16 @@ module.exports = () => {
 			registeredReceivers.push(receiver);
 
 			const onMessageHandler = async (brokeredMessage) => {
-				const { body, deliveryCount, userProperties } = brokeredMessage;
-
-				const schedule = async (message, scheduledTimeInMillisecs) => {
-					const client = connection.createTopicClient(topic);
-					registeredClients.push(client);
-					const sender = client.getSender();
-  				await sender.scheduleMessages(new Date(scheduledTimeInMillisecs), [message]);
-				};
-
-				const MAX_ATTEMPTS = 10;
-				const BACKOFF_FACTOR = 2;
-				const exponentialBackoff = async (msg, { options = { measure: 'minutes', attempts: MAX_ATTEMPTS } }) => {
-					// https://markheath.net/post/defer-processing-azure-service-bus-message
-					const attemptsLimit = (options.attempts > 0 && options.attempts <= 10) ? options.attempts : MAX_ATTEMPTS;
-					const { measure } = options;
-					const attempt = userProperties.attemptCount || deliveryCount;
-					if ((attempt + 1) === attemptsLimit) {
-						debug(`Maximum number of deliveries (${attemptsLimit}) reached on topic ${topic}. Sending to dlq...`);
-						await newErrorStrategies.deadLetter(topic)(brokeredMessage);
-					} else {
-						const nextAttempt = Math.pow(BACKOFF_FACTOR, attempt);
-						const scheduledTime = moment().add(nextAttempt, measure).toDate().getTime();
-						debug(`Retrying message exponentially with number of attempts ${attempt} on topic ${topic}. Scheduling for ${nextAttempt} ${measure}...`);
-						const clone = brokeredMessage.clone();
-						clone.userProperties = {
-							attemptCount: attempt + 1,
-						};
-						await Promise.all([
-							schedule(clone, scheduledTime),
-							brokeredMessage.complete(),
-						]);
-					}
-				};
 
 				const errorStrategies = {
 					retry: newErrorStrategies.retry(topic),
 					deadLetter: newErrorStrategies.deadLetter(topic),
-					exponentialBackoff,
+					exponentialBackoff: newErrorStrategies.exponentialBackoff(topic, connection),
 				};
 
 				try {
 					debug(`Handling message on topic ${topic}`);
-					await handler({ body, userProperties });
+					await handler({ body: brokeredMessage.body, userProperties: brokeredMessage.userProperties });
 					await brokeredMessage.complete();
 				} catch(e) {
 					const subscriptionErrorStrategy = (errorHandling || {}).strategy;
