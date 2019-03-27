1484222696b2d67bb53d5e09ecc870ed9efe54ca
Jose Antonio <josegrobles1997@gmail.com>
2019-03-27 13:33:58 +0200

    Retry sending a message when it fails (#5)
    * Add retry sending messages if first attempts fails
    
    * Upgrade dependencies
    
    * Add tests for publish function
    
    * Fix improve send test method

---
+++ test/unit/publish.test.js
@@ -0,0 +1,40 @@
+const publish = require('../../lib/operations/topics/publish');
+
+const createPayload = () => ({ foo: Date.now() });
+
+
+const createSender = (maxAttempts = 2) => {
+	let attempts = 0;
+
+	const send = async () => {
+		if (attempts < (maxAttempts - 1)) {
+			attempts++;
+			throw new Error();
+		}
+		return Promise.resolve();
+	};
+
+	return {
+		send,
+	};
+};
+
+
+describe('Publish  message on Topic', () => {
+	it('Should publish', () => new Promise(async resolve => {
+		const sender = createSender(3);
+		const publishMessage = publish(sender);
+		await publishMessage(createPayload());
+		resolve();
+	}));
+
+	it('Should not publish', () => new Promise(async resolve => {
+		const sender = createSender(5);
+		const publishMessage = publish(sender);
+		try {
+			await publishMessage(createPayload());
+		} catch (err) {
+			resolve();
+		}
+	}));
+});
